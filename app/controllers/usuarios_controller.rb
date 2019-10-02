443823560818e40c069071eefb187e7cd1c84592
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2019-10-01 20:20:51 -0600

    Migration autoincrement

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -5,10 +5,7 @@ class UsuariosController < ApplicationController
   # GET /usuarios.json
   def index
     @usuarios = Usuario.all
-    respond_to do |format|
-      format.html { @usuarios }
-      format.json { render json: @usuarios }
-    end
+    render json: @usuarios
   end
 
   # GET /usuarios/1
