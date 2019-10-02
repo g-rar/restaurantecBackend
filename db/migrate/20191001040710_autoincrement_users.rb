558ea2ad6685048ce4da6c314f8b39b3fce44813
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2019-10-02 02:47:34 -0600

    Deleted unneeded migration

--- db/migrate/20191001040710_autoincrement_users.rb
+++
@@ -1,6 +0,0 @@
-class AutoincrementUsers < ActiveRecord::Migration[6.0]
-  def change
-    execute 'ALTER TABLE usuarios drop idusuario'
-    execute 'ALTER TABLE usuarios add idusuario smallint NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY'
-  end
-end
